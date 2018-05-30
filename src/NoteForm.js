d2cd3d6e241eb4d06ef953f81417e073d3fc6f62
Sasha Kipnis <akipnis@purdue.edu>
2018-05-30 10:28:07 -0400

    Add state to NoteForm

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -2,45 +2,67 @@ import React from 'react'
 
 import './NoteForm.css'
 
-const NoteForm = ({currentNote, saveNote, handleDelete}) => {
-  const handleChanges = (ev) => {
-      const note = {...currentNote}
-      note[ev.target.name] = ev.target.value
-      saveNote(note)
+class NoteForm extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      note: this.blankNote,
+    }
   }
 
-  return (
-    <div className="NoteForm">
-      <div className="form-actions">
-        <button 
-          type="button"
-          onClick={() => handleDelete(currentNote)}
-        >
-          <i className="fas fa-trash-alt"></i>
-        </button>
-      </div>
-      <form>
-        <p>
-          <input
-            type="text"
-            name="title"
-            placeholder="Title your note..."
-            value={currentNote.title}
-            onChange={handleChanges}
-          />
-        </p>
+  blankNote = () => {
+    return {
+      id: null,
+      title: ' ',
+      body: ' ',
+    }
+  }
 
-        <textarea 
-          name="body"
-          placeholder="Start typing your note..."
-          value={currentNote.body}
-          onChange={handleChanges}
-        >
-          
-        </textarea>
-      </form>
-    </div>
-  )
+  handleChanges = (ev) => {
+      const note = {...this.state.note}
+      note[ev.target.name] = ev.target.value
+      this.setState(
+        {note},
+        () => this.props.saveNote(note)
+      )
+  }
+
+  render(){
+    const {currentNote, handleDelete} = this.props
+    return (
+      <div className="NoteForm">
+        <div className="form-actions">
+          <button 
+            type="button"
+            onClick={() => handleDelete(currentNote)}
+          >
+            <i className="fas fa-trash-alt"></i>
+          </button>
+        </div>
+        <form>
+          <p>
+            <input
+              type="text"
+              name="title"
+              placeholder="Title your note..."
+              value={this.state.note.title}
+              onChange={this.handleChanges}
+            />
+          </p>
+
+          <textarea 
+            name="body"
+            placeholder="Start typing your note..."
+            value={this.state.note.body}
+            onChange={this.handleChanges}
+          >
+            
+          </textarea>
+        </form>
+      </div>
+    )
+}
 }
 
 export default NoteForm
